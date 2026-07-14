554c51d5751cb5174b171dd0ae9eace0ba7fb677
het jiyani <hetjiyani24@gmail.com>
2026-07-14 22:49:20 +0530

    u

--- pro1/Main.java
+++ pro1/Main.java
@@ -1,109 +1,115 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         User user = new User();
         Hackathon hackathon = new Hackathon();
+        Team team = new Team();
+        Registration registration = new Registration();
 
         int choice;
+        String userEmail = "";
 
         do {
+
             System.out.println("\n========== HACKATHON PORTAL ==========");
-            System.out.println("1. User Login");
-            System.out.println("2. Organization Login");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
             System.out.println("3. Exit");
             System.out.print("Enter Choice: ");
+
             choice = sc.nextInt();
             sc.nextLine();
 
             switch (choice) {
 
-                // ---------------- USER ----------------
                 case 1:
-                    int userChoice;
-
-                    do {
-                        System.out.println("\n------ USER MENU ------");
-                        System.out.println("1. Register User");
-                        System.out.println("2. Edit Profile");
-                        System.out.println("3. View Profile");
-                        System.out.println("4. View Hackathon");
-                        System.out.println("5. Logout");
-                        System.out.print("Enter Choice: ");
-
-                        userChoice = sc.nextInt();
-                        sc.nextLine();
-
-                        switch (userChoice) {
-
-                            case 1:
-                                user.addNewUser();
-                                break;
-
-                            case 2:
-                                user.editProfile();
-                                break;
-
-                            case 3:
-                                user.viewProfile();
-                                break;
-
-                            case 4:
-                                hackathon.viewHackathon();
-                                break;
-
-                            case 5:
-                                System.out.println("Logged Out Successfully.");
-                                break;
-
-                            default:
-                                System.out.println("Invalid Choice!");
-                        }
-
-                    } while (userChoice != 5);
+                    user.registerUser();
                     break;
 
-                // ---------------- ORGANIZATION ----------------
                 case 2:
-                    int orgChoice;
 
-                    do {
-                        System.out.println("\n------ ORGANIZATION MENU ------");
-                        System.out.println("1. Add Hackathon");
-                        System.out.println("2. View Hackathon");
-                        System.out.println("3. Delete Hackathon");
-                        System.out.println("4. Logout");
-                        System.out.print("Enter Choice: ");
+                    if (user.loginUser()) {
 
-                        orgChoice = sc.nextInt();
-                        sc.nextLine();
+                        System.out.print("Enter Your Email Again: ");
+                        userEmail = sc.nextLine();
 
-                        switch (orgChoice) {
+                        int userChoice;
 
-                            case 1:
-                                hackathon.addHackathon();
-                                break;
+                        do {
 
-                            case 2:
-                                hackathon.viewHackathon();
-                                break;
+                            System.out.println("\n========== USER MENU ==========");
+                            System.out.println("1. Edit Profile");
+                            System.out.println("2. View Profile");
+                            System.out.println("3. View Hackathons");
+                            System.out.println("4. Create Team");
+                            System.out.println("5. Join Team");
+                            System.out.println("6. Leave Team");
+                            System.out.println("7. View Teams");
+                            System.out.println("8. View Team Members");
+                            System.out.println("9. Register for Hackathon");
+                            System.out.println("10. Logout");
+                            System.out.print("Enter Choice: ");
 
-                            case 3:
-                                hackathon.deleteHackathon();
-                                break;
+                            userChoice = sc.nextInt();
+                            sc.nextLine();
 
-                            case 4:
-                                System.out.println("Logged Out Successfully.");
-                                break;
+                            switch (userChoice) {
 
-                            default:
-                                System.out.println("Invalid Choice!");
-                        }
+                                case 1:
+                                    user.editProfile();
+                                    break;
 
-                    } while (orgChoice != 4);
+                                case 2:
+                                    user.viewProfile();
+                                    break;
+
+                                case 3:
+                                    hackathon.viewHackathon();
+                                    break;
+
+                                case 4:
+                                    team.createTeam(userEmail);
+                                    break;
+
+                                case 5:
+                                    team.joinTeam(userEmail);
+                                    break;
+
+                                case 6:
+                                    team.leaveTeam(userEmail);
+                                    break;
+
+                                case 7:
+                                    team.viewTeam();
+                                    break;
+
+                                case 8:
+                                    team.viewTeamMembers();
+                                    break;
+
+                                case 9:
+                                    registration.registerHackathon(userEmail);
+                                    break;
+
+                                case 10:
+                                    System.out.println("Logged Out Successfully!");
+                                    break;
+
+                                default:
+                                    System.out.println("Invalid Choice!");
+                            }
+
+                        } while (userChoice != 10);
+
+                    } else {
+                        System.out.println("Login Failed!");
+                    }
+
                     break;
 
                 case 3:
