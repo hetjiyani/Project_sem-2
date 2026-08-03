1c6fdd1806d08b3bdf455979e7420031b2b16fdf
het jiyani <hetjiyani24@gmail.com>
2026-08-03 18:35:55 +0530

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,9 +1,4 @@
-package pro1;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Team {
